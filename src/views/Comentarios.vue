3e6cad36fc13063793aadf539f8c7957737e1c70
Manuel Riquelme <clonbg@gmail.com>
2020-05-10 15:21:33 +0200

    Disqus

--- src/views/Comentarios.vue
+++ src/views/Comentarios.vue
@@ -1,31 +1,25 @@
 <template>
-  
-<div id="graphcomment"></div>
+
+  <div>
+    <div id="disqus_thread"></div>
+    <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+  </div>
+
 </template>
 
 <script>
-export default {
-    name: "Comentarios",
-    created(){
-        /* - - - CONFIGURATION VARIABLES - - - */
 
-    // make sure the id is yours
-    window.gc_params = {
-        graphcomment_id: 'La-web-de-clonbg',
-
-        // if your website has a fixed header, indicate it's height in pixels
-        fixed_header_height: 0,
-    };
-
-    /* - - - DON'T EDIT BELOW THIS LINE - - - */
-
-    
-    (function() {
-        var gc = document.createElement('script'); gc.type = 'text/javascript'; gc.async = true;
-        gc.src = 'https://graphcomment.com/js/integration.js?' + Math.round(Math.random() * 1e8);
-        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(gc);
-    })();
-
+  export default {
+    name: 'Comentarios',
+    created() {
+      ;(function() {
+        var d = document,
+          s = d.createElement('script')
+        s.src = 'https://https-silly-goldberg-68d2eb-netlify-app.disqus.com/embed.js'
+        s.setAttribute('data-timestamp', +new Date())
+        ;(d.head || d.body).appendChild(s)
+      })()
     }
-}
+  }
+
 </script>

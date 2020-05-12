61594e2fe3660018670dec6f6b5b36b73875967a
Manuel Riquelme <clonbg@gmail.com>
2020-05-12 17:39:38 +0200

    Abre menos el menú

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
+
   <div id="app">
     <div>
-      <Menu style="display:flex;"/>
+      <Menu class="menu" />
       <router-view v-if="!$route.meta.keepAlive"></router-view>
       <keep-alive>
         <router-view v-if="$route.meta.keepAlive"></router-view>
@@ -10,27 +11,38 @@
       <Footer />
     </div>
   </div>
+
 </template>
 
 <script>
-import Footer from "@/views/Footer.vue";
-import Menu from "@/views/Menu.vue";
-export default {
-  name: "app",
-  components: {
-    Footer,
-    Menu
+
+  import Footer from '@/views/Footer.vue'
+  import Menu from '@/views/Menu.vue'
+  export default {
+    name: 'app',
+    components: {
+      Footer,
+      Menu
+    }
   }
-};
+
 </script>
 
 <style scoped>
-#app {
-  font-family: "Ubuntu", "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  max-width: 120vw;
-  margin: 0 auto;
-}
+
+  #app {
+    font-family: 'Ubuntu', 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    max-width: 120vw;
+    margin: 0 auto;
+  }
+  .menu {
+    display: flex;
+    margin: 0 auto;
+    min-width: min-content;
+    max-width: 100vh;
+  }
+
 </style>

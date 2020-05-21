3692d160c03e5a1431e72e9385a61e8cd47d6484
Manuel Riquelme <clonbg@gmail.com>
2020-05-21 18:25:38 +0200

    Sombra hover logo eliminada

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -2,14 +2,14 @@
 
   <div>
 
-    <md-menu md-direction="bottom-start" class="posicionLogo">
+    <md-menu md-direction="bottom-start" class="posicionLogo"  @click="$router.push({name: 'home'})">
       <md-button md-menu-trigger>
-        <img src="../assets/logo_clonbg_es.jpg" @click="$router.push({name: 'home'})" class="img-fluid logo" alt="Logo" />
+        <img src="../assets/logo_clonbg_es.jpg" class="logo" alt="Logo" />
       </md-button>
     </md-menu>
 
     <md-menu md-direction="bottom-end">
-      <md-button md-menu-trigger>
+      <md-button md-menu-trigger disabled>
         <h1 @click="$router.push({name: 'home'})">El blog de Clonbg</h1>
       </md-button>
     </md-menu>
@@ -17,17 +17,17 @@
 
     </div>
     <!-- <md-menu md-direction="top-start" @click="$router.push({name: 'acerca'})" class="posicionAcerca">Acerca de</md-button>
-    </md-menu> -->
+        </md-menu> -->
 
     <md-menu md-direction="top-end" class="rssCabecera">
       <md-button md-menu-trigger><a :href="`${publicPath}feedClonbg_es.xml`" target="_blank">
-            <img class="tamanoRss" src="@/icons/rss2.svg" width="100"/>
-          </a></md-button>
+                <img class="tamanoRss" src="@/icons/rss2.svg" width="100"/>
+              </a></md-button>
     </md-menu>
 
     <!-- <md-menu md-direction="top-end">
-            <md-button md-menu-trigger>Top End</md-button>
-          </md-menu> -->
+                <md-button md-menu-trigger>Top End</md-button>
+              </md-menu> -->
   </div>
 
 </template>
@@ -45,7 +45,7 @@
 
 </script>
 
-<style lang="scss" scoped>
+<style lang="css" scoped>
 
   .md-menu {
     margin: 24px;
@@ -61,9 +61,12 @@
   .logo {
     max-width: 60px;
   }
+  .nohover:hover {
+    border: 0;
+  }
   .posicionLogo {
     float: left;
-    margin-right: -25px;
+    margin-right: 12px;
   }
   .posicionAcerca {
     float: right;
@@ -72,9 +75,14 @@
   }
   h1 {
     font-size: 2vh;
+    cursor: none;
+    color: black;
   }
   .espaciador {
     width: 100%;
   }
-
+  .md-button:not([disabled]).md-focused::before, .md-button:not([disabled]):active::before, .md-button:not([disabled]):hover::before {
+    background-color: white;
+    opacity: .12;
+  }
 </style>
